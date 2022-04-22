2851984d6f436d42f7c72e6ca733eddbe794c170
salih abas <salihabas16@gmail.com>
2022-04-22 21:32:27 +0300

    AC1 test completed

--- src/LoginTest_Salih.java
+++ src/LoginTest_Salih.java
@@ -2,5 +2,8 @@ public class LoginTest_Salih {
     public static void main(String[] args) {
 
         System.out.println("Initial and first class");
+
+        System.out.println("AC1 Test is passed");
+
     }
 }
